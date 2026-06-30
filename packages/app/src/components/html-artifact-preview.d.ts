c4d5451727108e599aeee777382d486129aa16a6
Vivek Olumbe <olumbe@seas.upenn.edu>
2026-06-30 08:37:10 -0700

    Render HTML artifact file links

---
+++ packages/app/src/components/html-artifact-preview.d.ts
@@ -0,0 +1,8 @@
+import type { ReactElement } from "react";
+
+interface HtmlArtifactPreviewProps {
+  html: string;
+  filePath: string;
+}
+
+export function HtmlArtifactPreview(props: HtmlArtifactPreviewProps): ReactElement;
